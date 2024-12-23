ce96257dc73fc3be84f157166ed69ef0012a02f1
pragmaticLudusian <arsen.r.m@gmail.com>
2024-12-23 22:39:46 +0200

    feat: implement Main component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <div className="main">
+      <p>test</p>
+    </div>
+  );
+}
+
+export default Main;
