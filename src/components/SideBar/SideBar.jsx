3eb8d5c5055c33e80db1a81edc54faee97d9b9ce
pragmaticLudusian <arsen.r.m@gmail.com>
2025-02-12 01:55:41 +0200

    feat: implement (hardcoded) SideBar component, adjust padding delegation to main block

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,12 @@
 import "./SideBar.css";
 
 function SideBar() {
-  return <aside className="side-bar">Cider</aside>;
+  return (
+    <aside className="side-bar">
+      <div className="side-bar__avatar side-bar__avatar_image" />
+      <p className="side-bar__username">Terrence Tegegne</p>
+    </aside>
+  );
 }
 
 export default SideBar;
