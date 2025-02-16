16d7b6c8bbe8ac6b7f93ccbaf6d9c2728ac0c743
pragmaticLudusian <arsen.r.m@gmail.com>
2025-02-16 22:14:44 +0200

    feat: adjust mobile design for profile sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,7 +4,17 @@ function SideBar() {
   return (
     <aside className="side-bar">
       <div className="side-bar__avatar side-bar__avatar_image" />
-      <p className="side-bar__username">Terrence Tegegne</p>
+      <div className="side-bar__user-container">
+        <p className="side-bar__username">Terrence Tegegne</p>
+        <div className="side-bar__user-options">
+          <button type="button" className="side-bar__user-option">
+            Change profile data
+          </button>
+          <button type="button" className="side-bar__user-option">
+            Log out
+          </button>
+        </div>
+      </div>
     </aside>
   );
 }
