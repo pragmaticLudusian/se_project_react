b03b6cc54bb055377cce0eb6c92111a49835aa28
pragmaticLudusian <arsen.r.m@gmail.com>
2025-02-19 00:17:55 +0200

    feat: fine-tune card header orientation, separate name tag from desktop and mobile, add light close variant

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -21,18 +21,21 @@ function ItemModal(props) {
         <div className="item-modal__card-container">
           <img src={imageUrl} alt={name} className="item-modal__image" />
           <div className="item-modal__header">
-            <p className="item-modal__name">{name}</p>
+            <p className="item-modal__header-name">{name}</p>
           </div>
         </div>
         <div className="item-modal__text-container">
           <p className="item-modal__description">Weather: {weather}</p>
-          <button
-            type="button"
-            className="item-modal__delete-btn"
-            onClick={props.openDeleteConfirm}
-          >
-            Delete item
-          </button>
+          <div className="item-modal__text-name-container">
+            <p className="item-modal__text-name">{name}</p>
+            <button
+              type="button"
+              className="item-modal__delete-btn"
+              onClick={props.openDeleteConfirm}
+            >
+              Delete item
+            </button>
+          </div>
         </div>
       </div>
     </div>
