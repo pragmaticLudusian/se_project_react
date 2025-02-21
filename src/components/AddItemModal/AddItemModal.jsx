1f1652e61c9b09e1dace3fa01594ebbac36db67e
pragmaticLudusian <arsen.r.m@gmail.com>
2025-02-21 16:49:16 +0200

    refactor: move form validation into custom hook

--- src/components/AddItemModal/AddItemModal.jsx
+++ src/components/AddItemModal/AddItemModal.jsx
@@ -1,61 +1,19 @@
 import { useEffect, useState } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useFormAndValidation } from "../../utils/customHooks";
 // AddItemModal is considered to be extended from ModalWithForm, despite MWF component contained *within* AIM
 
 function AddItemModal({ onAddItem, onClose, buttonText }) {
-  const [name, setName] = useState({
-    value: "",
-    isValid: false,
-    validationMessage: "",
-  });
-  const [imageUrl, setImageUrl] = useState({
-    value: "",
-    isValid: false,
-    validationMessage: "",
-  });
-  const [weather, setWeather] = useState({
-    value: "",
-    isValid: false,
-    validationMessage: "",
-  }); // because the form submission button is disabled and radio cannot be unset, validation msg is pointless to implement here
-  const [isFormValid, setFormValid] = useState(false);
+  const { values, errors, isValid, handleChange, resetForm } =
+    useFormAndValidation();
 
   useEffect(() => {
-    setName({ value: "" });
-    setImageUrl({ value: "" });
-    setWeather({ value: "" });
+    resetForm();
   }, []);
-
-  function handleName(event) {
-    setName(validateForm(event.target));
-  }
-
-  function handleLink(event) {
-    setImageUrl(validateForm(event.target));
-  }
-
-  function handleWeather(event) {
-    setWeather(validateForm(event.target));
-  }
-
-  function validateForm(input) {
-    setFormValid(input.closest("form").checkValidity()); // handle global form validation too
-    return {
-      value: input.value,
-      isValid: input.validity.valid,
-      validationMessage:
-        input.validationMessage && `(${input.validationMessage})`,
-    };
-  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const formObject = {
-      name: name.value,
-      imageUrl: imageUrl.value,
-      weather: weather.value,
-    }; // alt w/out state can be Object.fromEntries(FormData.entries())
-    onAddItem(formObject); // close modal from there inside .then
+    onAddItem(values); // close modal from there inside .then
   }
 
   return (
@@ -65,44 +23,44 @@ function AddItemModal({ onAddItem, onClose, buttonText }) {
       buttonText={buttonText}
       onClose={onClose}
       onSubmit={handleSubmit}
-      isValid={isFormValid}
+      isValid={isValid}
     >
       <label
         className={`form-modal__label ${
-          name.validationMessage ? "form-modal__label_error" : ""
+          errors.name ? "form-modal__label_error" : ""
         }`}
       >
-        Name {!name.isValid && name.validationMessage}
+        Name {errors.name && `(${errors.name})`}
         <input
           type="text"
           className={`form-modal__input form-modal__input_type_text ${
-            name.validationMessage ? "form-modal__input_error" : ""
+            errors.name ? "form-modal__input_error" : ""
           }`}
           name="name"
           id="add-clothes_name" // used with htmlFor, unless nested inside label
           placeholder="Name"
           required
-          onChange={handleName}
-          value={name.value}
+          onChange={handleChange}
+          value={values.name || ""} // initially it would not only be outdated but also be undef, which can cause conflict between (un)controlling the input. the || resolves this
         />
       </label>
       <label
         className={`form-modal__label ${
-          imageUrl.validationMessage ? "form-modal__label_error" : ""
+          errors.imageUrl ? "form-modal__label_error" : ""
         }`}
       >
-        Image {!imageUrl.isValid && imageUrl.validationMessage}
+        Image {errors.imageUrl && `(${errors.imageUrl})`}
         <input
           type="url"
           className={`form-modal__input form-modal__input_type_text ${
-            imageUrl.validationMessage ? "form-modal__input_error" : ""
+            errors.imageUrl ? "form-modal__input_error" : ""
           }`}
           name="imageUrl"
           id="add-clothes_imageUrl"
           placeholder="Image URL"
           required
-          onChange={handleLink}
-          value={imageUrl.value}
+          onChange={handleChange}
+          value={values.imageUrl || ""}
         />
       </label>
       <fieldset className="form-modal__fieldset">
@@ -115,10 +73,10 @@ function AddItemModal({ onAddItem, onClose, buttonText }) {
             name="weather"
             id="hot"
             value="hot"
-            checked={weather.value === "hot"}
+            checked={values.weather === "hot"}
             className="form-modal__input form-modal__input_type_radio"
             required // for at least one radio
-            onChange={handleWeather}
+            onChange={handleChange}
           />
           <label htmlFor="hot" className="form-modal__radio-label">
             Hot
@@ -130,9 +88,9 @@ function AddItemModal({ onAddItem, onClose, buttonText }) {
             name="weather"
             id="warm"
             value="warm"
-            checked={weather.value === "warm"}
+            checked={values.weather === "warm"}
             className="form-modal__input form-modal__input_type_radio"
-            onChange={handleWeather}
+            onChange={handleChange}
           />
           <label htmlFor="warm" className="form-modal__radio-label">
             Warm
@@ -144,9 +102,9 @@ function AddItemModal({ onAddItem, onClose, buttonText }) {
             name="weather"
             id="cool"
             value="cool"
-            checked={weather.value === "cool"}
+            checked={values.weather === "cool"}
             className="form-modal__input form-modal__input_type_radio"
-            onChange={handleWeather}
+            onChange={handleChange}
           />
           <label htmlFor="cool" className="form-modal__radio-label">
             Cool
@@ -158,9 +116,9 @@ function AddItemModal({ onAddItem, onClose, buttonText }) {
             name="weather"
             id="cold"
             value="cold"
-            checked={weather.value === "cold"}
+            checked={values.weather === "cold"}
             className="form-modal__input form-modal__input_type_radio"
-            onChange={handleWeather}
+            onChange={handleChange}
           />
           <label htmlFor="cold" className="form-modal__radio-label">
             Cold
--- src/utils/customHooks.js
+++ src/utils/customHooks.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function useEscape(closeModal) {
   useEffect(() => {
@@ -13,4 +13,38 @@ export function useEscape(closeModal) {
       window.removeEventListener("keydown", handleEscape);
     };
   }, [closeModal]);
+}
+
+export function useFormAndValidation() {
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isValid, setValid] = useState(false);
+
+  function handleChange(event) {
+    const { name, value, validationMessage } = event.target;
+    setValues((oldValues) => {
+      return { ...oldValues, [name]: value };
+    });
+    setErrors((oldErrors) => {
+      return { ...oldErrors, [name]: validationMessage };
+    });
+    setValid(event.target.closest("form").checkValidity());
+  }
+
+  const resetForm = useCallback(
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
+      setValues(newValues);
+      setErrors(newErrors);
+      setValid(newIsValid);
+    },
+    [setValues, setErrors, setValid]
+  );
+
+  return {
+    values,
+    errors,
+    isValid,
+    handleChange,
+    resetForm,
+  };
 }
